8c46708bb27dbb1b6c44e5e1e5ee0795503b5dc2
Pawel Szafer <pszafer@gmail.com>
2019-09-02 09:05:23 +0200

    first upgrade for operation mode

--- bosch/climate.py
+++ bosch/climate.py
@@ -1,20 +1,25 @@
 """Support for Bosch Thermostat Climate."""
 import logging
 
-from bosch_thermostat_http.const import (OPERATION_MODE, STATUS,
-                                         SYSTEM_BRAND, SYSTEM_TYPE,
-                                         ALLOWED_VALUES)
+from bosch_thermostat_http.const import (
+    OPERATION_MODE,
+    STATUS,
+    SYSTEM_BRAND,
+    SYSTEM_TYPE,
+    ALLOWED_VALUES,
+)
 
 from homeassistant.components.climate import ClimateDevice
-from homeassistant.components.climate.const import (HVAC_MODE_AUTO,
-                                                    HVAC_MODE_HEAT,
-                                                    HVAC_MODE_OFF,
-                                                    SERVICE_SET_HVAC_MODE,
-                                                    SUPPORT_TARGET_TEMPERATURE)
+from homeassistant.components.climate.const import (
+    HVAC_MODE_AUTO,
+    HVAC_MODE_HEAT,
+    HVAC_MODE_OFF,
+    SERVICE_SET_HVAC_MODE,
+    SUPPORT_TARGET_TEMPERATURE,
+)
 from homeassistant.const import ATTR_TEMPERATURE, TEMP_CELSIUS, TEMP_FAHRENHEIT
 
-from .const import (DOMAIN, GATEWAY, HCS, SIGNAL_CLIMATE_UPDATE_BOSCH,
-                    BOSCH_GW_ENTRY)
+from .const import DOMAIN, GATEWAY, HCS, SIGNAL_CLIMATE_UPDATE_BOSCH, BOSCH_GW_ENTRY
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -23,15 +28,16 @@ async def async_setup_entry(hass, config_entry, async_add_entities):
     """Set up the Bosch thermostat from a config entry."""
     uuid = config_entry.title
     data = hass.data[DOMAIN][uuid]
-    data[HCS] = [BoschThermostat(hass, uuid, hc, data[GATEWAY])
-                 for hc in data[GATEWAY].heating_circuits]
+    data[HCS] = [
+        BoschThermostat(hass, uuid, hc, data[GATEWAY])
+        for hc in data[GATEWAY].heating_circuits
+    ]
     async_add_entities(data[HCS])
     await data[BOSCH_GW_ENTRY].climate_refresh()
     return True
 
 
-async def async_setup_platform(hass, config, async_add_entities,
-                               discovery_info=None):
+async def async_setup_platform(hass, config, async_add_entities, discovery_info=None):
     """Set up the Bosch Thermostat Platform."""
     pass
 
@@ -52,62 +58,32 @@ class BoschThermostat(ClimateDevice):
         self._state = None
         self._operation_list = None
         self._uuid = uuid
-        self._unique_id = self._name+self._uuid
+        self._unique_id = self._name + self._uuid
         self._gateway = gateway
-        self._all_modes = self.all_modes_dict()
-        (self._op_modes, self._op_modes_inv) = self.prepare_op_modes()
+        self._hastates, self._hastates_inv = (
+            self._hc.hastates,
+            {value: key for key, value in self._hc.hastates.items()},
+        )
+        self._target_temperature = None
+        self._operation_list = []
         self._update_init = True
 
     async def async_added_to_hass(self):
         """Register callbacks."""
         self.hass.helpers.dispatcher.async_dispatcher_connect(
-            SIGNAL_CLIMATE_UPDATE_BOSCH, self.update)
-
-    def all_modes_dict(self):
-        modes_inv = {
-            self._hc.strings.auto: HVAC_MODE_AUTO,
-            self._hc.strings.manual: HVAC_MODE_HEAT,
-            self._hc.strings.off: HVAC_MODE_OFF,
-            self._hc.strings.on: HVAC_MODE_HEAT,
-        }
-        return modes_inv
-
-    def prepare_op_modes(self, bosch_modes=None):
-        if not bosch_modes:
-            op_modes = {
-                HVAC_MODE_AUTO: self._hc.strings.auto,
-                # HVAC_MODE_HEAT: self._hc.strings.manual
-            }
-            op_modes_inv = {v: k for k, v in op_modes.items()}
-            return (op_modes, op_modes_inv)
-        # print("BOsch modes ", bosch_modes)
-        # print("ALL MODES: ", self._all_modes)
-        op_modes = {}
-        op_modes_inv = {}
-        for mode in bosch_modes:
-            mode_string = self._hc.strings.get(mode)
-            ha_mode = self._all_modes.get(mode_string)
-            # print("Looking for mode ", mode)
-            # print("Mode strings: ", mode_string)
-            # print("HA MOde translation: ", ha_mode)
-            if ha_mode and mode_string:
-                op_modes[ha_mode] = mode_string
-                op_modes_inv[mode_string] = ha_mode
-        print("FINITO")
-        return (op_modes, op_modes_inv)
+            SIGNAL_CLIMATE_UPDATE_BOSCH, self.update
+        )
 
     @property
     def device_info(self):
         """Get attributes about the device."""
         return {
-            'identifiers': {
-                (DOMAIN, self._unique_id)
-            },
-            'manufacturer': self._gateway.get_info(SYSTEM_BRAND),
-            'model': self._gateway.get_info(SYSTEM_TYPE),
-            'name': 'Heating circuit ' + self._name,
-            'sw_version': self._gateway.firmware,
-            'via_hub': (DOMAIN, self._uuid)
+            "identifiers": {(DOMAIN, self._unique_id)},
+            "manufacturer": self._gateway.get_info(SYSTEM_BRAND),
+            "model": self._gateway.get_info(SYSTEM_TYPE),
+            "name": "Heating circuit " + self._name,
+            "sw_version": self._gateway.firmware,
+            "via_hub": (DOMAIN, self._uuid),
         }
 
     @property
@@ -157,55 +133,58 @@ class BoschThermostat(ClimateDevice):
 
     async def async_set_hvac_mode(self, hvac_mode):
         """Set operation mode."""
-        _LOGGER.debug("Setting operation mode %s.", hvac_mode)
-        op_mode = self._op_modes.get(hvac_mode)
-        _LOGGER.debug("Setting Bosch operation mode %s.", op_mode)
-        if op_mode and op_mode != self._mode.get(self._hc.strings.val):
-            self._mode[self._hc.strings.val] = await\
-                self._hc.set_operation_mode(op_mode)
-            _LOGGER.debug("Set operation mode to %s.",
-                          self._mode[self._hc.strings.val])
+        op_mode = self._hastates.get(hvac_mode)
+        _LOGGER.debug(f"Setting operation mode {hvac_mode} which is Bosch {op_mode}.")
+        if op_mode and hvac_mode != self._mode and hvac_mode in self._operation_list:
+            new_mode = await self._hc.set_operation_mode(op_mode)
+            _LOGGER.debug(f"Set operation mode to {new_mode}.")
+            # if new_mode:
+            #     self._mode = self._hastates_inv.get(new_mode, HVAC_MODE_OFF)
             return True
-        self._mode = self._hc.get_property(OPERATION_MODE)
         return False
 
     async def async_set_temperature(self, **kwargs):
         """Set new target temperature."""
         if self._hc.target_temperature:
             temperature = kwargs.get(ATTR_TEMPERATURE)
-            if temperature is None:
-                return
             if temperature != self._hc.target_temperature:
                 await self._hc.set_temperature(temperature)
-            await self.async_update_ha_state()
 
     @property
     def hvac_mode(self):
         """Return current operation ie. heat, cool, idle."""
-        return self._op_modes_inv.get(self._mode.get(self._hc.strings.val),
-                                      HVAC_MODE_OFF)
+        return self._mode
 
     @property
     def hvac_modes(self):
         """List of available operation modes."""
-        return list(self._op_modes.keys())
+        return self._operation_list
 
     def update(self):
         """Update state of device."""
         _LOGGER.debug("Update of climate component called.")
-        if (not self._hc or not self._hc.json_scheme_ready or
-                not self._hc.update_initialized):
+        if (
+            not self._hc
+            or not self._hc.json_scheme_ready
+            or not self._hc.update_initialized
+        ):
             return
         self._state = self._hc.get_value(STATUS)
-        self._temperature_unit = (TEMP_FAHRENHEIT if self._hc.temp_units == 'F'
-                                  else TEMP_CELSIUS)
+        self._temperature_unit = (
+            TEMP_FAHRENHEIT if self._hc.temp_units == "F" else TEMP_CELSIUS
+        )
+
         # self._holiday_mode = self._hc.get_value(HC_HOLIDAY_MODE)
-        self._mode = self._hc.get_property(OPERATION_MODE)
-        allowed_values = self._mode.get(ALLOWED_VALUES)
-        # allowed_values = ["on", "off", "auto"]
-        (self._op_modes, self._op_modes_inv) = self.prepare_op_modes(
-            allowed_values)
+        mode = self._hc.get_property(OPERATION_MODE)
+        self._operation_list = self.operation_list_converter(
+            mode.get(self._hc.strings.allowed_values)
+        )
+        self._mode = self._hastates_inv.get(
+            mode.get(self._hc.strings.val, HVAC_MODE_OFF))
         if self._update_init:
             self._update_init = False
             self.async_schedule_update_ha_state()
-        _LOGGER.debug("Retrieved mode %s", self._mode)
+        _LOGGER.debug(f"Retrieved mode {self._mode}")
+
+    def operation_list_converter(self, op_list):
+        return [self._hastates_inv.get(value) for value in op_list]
--- bosch/water_heater.py
+++ bosch/water_heater.py
@@ -5,25 +5,38 @@ For more details about this platform, please refer to the documentation at...
 """
 import logging
 
-from bosch_thermostat_http.const import (GATEWAY, OPERATION_MODE, SYSTEM_BRAND,
-                                         SYSTEM_TYPE, CURRENT_TEMP, WATER_OFF,
-                                         STATUS)
+from bosch_thermostat_http.const import (
+    GATEWAY,
+    OPERATION_MODE,
+    SYSTEM_BRAND,
+    SYSTEM_TYPE,
+    CURRENT_TEMP,
+    WATER_OFF,
+    STATUS,
+)
 
-from homeassistant.components.water_heater import (STATE_HEAT_PUMP,
-                                                   STATE_HIGH_DEMAND,
-                                                   STATE_OFF,
-                                                   STATE_PERFORMANCE,
-                                                   SUPPORT_OPERATION_MODE,
-                                                   SUPPORT_TARGET_TEMPERATURE,
-                                                   WaterHeaterDevice)
+from homeassistant.components.water_heater import (
+    STATE_HEAT_PUMP,
+    STATE_HIGH_DEMAND,
+    STATE_OFF,
+    STATE_PERFORMANCE,
+    SUPPORT_OPERATION_MODE,
+    SUPPORT_TARGET_TEMPERATURE,
+    WaterHeaterDevice,
+)
 from homeassistant.const import ATTR_TEMPERATURE, TEMP_CELSIUS
 
-from .const import (DHWS, DOMAIN, SIGNAL_DHW_UPDATE_BOSCH, UNITS_CONVERTER,
-                    BOSCH_GW_ENTRY)
+from .const import (
+    DHWS,
+    DOMAIN,
+    SIGNAL_DHW_UPDATE_BOSCH,
+    UNITS_CONVERTER,
+    BOSCH_GW_ENTRY,
+)
 
 _LOGGER = logging.getLogger(__name__)
 
-SUPPORT_FLAGS_HEATER = (SUPPORT_TARGET_TEMPERATURE | SUPPORT_OPERATION_MODE)
+SUPPORT_FLAGS_HEATER = SUPPORT_TARGET_TEMPERATURE | SUPPORT_OPERATION_MODE
 
 DEFAULT_MIN_TEMP = 0
 DEFAULT_MAX_TEMP = 100
@@ -33,28 +46,18 @@ async def async_setup_entry(hass, config_entry, async_add_entities):
     """Set up the Bosch Water heater from a config entry."""
     uuid = config_entry.title
     data = hass.data[DOMAIN][uuid]
-    data[DHWS] = [BoschWaterHeater(hass, uuid, dhw, data[GATEWAY])
-                  for dhw in data[GATEWAY].dhw_circuits]
+    data[DHWS] = [
+        BoschWaterHeater(hass, uuid, dhw, data[GATEWAY])
+        for dhw in data[GATEWAY].dhw_circuits
+    ]
     async_add_entities(data[DHWS])
     await data[BOSCH_GW_ENTRY].water_heater_refresh()
     return True
 
 
-async def async_setup_platform(hass, config, async_add_entities,
-                               discovery_info=None):
+async def async_setup_platform(hass, config, async_add_entities, discovery_info=None):
     """Set up the Bosch Thermostat Platform."""
     pass
-
-
-def bosch_states(strings):
-    HA_STATE_TO_BOSCH = {
-        STATE_HEAT_PUMP: strings.hcprogram,
-        STATE_HIGH_DEMAND: strings.ownprogram,
-        STATE_OFF:  strings.off,
-        STATE_PERFORMANCE: 'high'
-    }
-    B_TO_HA = {value: key for key, value in HA_STATE_TO_BOSCH.items()}
-    return HA_STATE_TO_BOSCH, B_TO_HA
 
 
 class BoschWaterHeater(WaterHeaterDevice):
@@ -66,7 +69,7 @@ class BoschWaterHeater(WaterHeaterDevice):
         self._dhw = dhw
         self._name = self._dhw.name
         self._uuid = uuid
-        self._unique_id = self._name+self._uuid
+        self._unique_id = self._name + self._uuid
         self._gateway = gateway
         self._mode = None
         self._state = None
@@ -78,14 +81,17 @@ class BoschWaterHeater(WaterHeaterDevice):
         self._low_temp = DEFAULT_MIN_TEMP
         self._target_temp_off = 0
         self._operation_list = []
-        self._states_conv, self._states_conv_inv = \
-            bosch_states(self._dhw.strings)
+        self._hastates, self._hastates_inv = (
+            self._dhw.hastates,
+            {value: key for key, value in self._dhw.hastates.items()},
+        )
         self._update_init = True
 
     async def async_added_to_hass(self):
         """Register callbacks."""
         self.hass.helpers.dispatcher.async_dispatcher_connect(
-            SIGNAL_DHW_UPDATE_BOSCH, self.update)
+            SIGNAL_DHW_UPDATE_BOSCH, self.update
+        )
 
     @property
     def name(self):
@@ -106,14 +112,12 @@ class BoschWaterHeater(WaterHeaterDevice):
     def device_info(self):
         """Get attributes about the device."""
         return {
-            'identifiers': {
-                (DOMAIN, self._unique_id)
-            },
-            'manufacturer': self._gateway.get_info(SYSTEM_BRAND),
-            'model': self._gateway.get_info(SYSTEM_TYPE),
-            'name': 'Water heater ' + self._name,
-            'sw_version': self._gateway.firmware,
-            'via_hub': (DOMAIN, self._uuid)
+            "identifiers": {(DOMAIN, self._unique_id)},
+            "manufacturer": self._gateway.get_info(SYSTEM_BRAND),
+            "model": self._gateway.get_info(SYSTEM_TYPE),
+            "name": "Water heater " + self._name,
+            "sw_version": self._gateway.firmware,
+            "via_hub": (DOMAIN, self._uuid),
         }
 
     @property
@@ -129,9 +133,7 @@ class BoschWaterHeater(WaterHeaterDevice):
     @property
     def device_state_attributes(self):
         """Return the optional device state attributes."""
-        data = {
-            "target_temp_step": 1
-        }
+        data = {"target_temp_step": 1}
         # data["CALENDAR"] = self._dhw.get_schedule
         # vacations = self.water_heater.get_vacations()
         # if vacations:
@@ -166,44 +168,55 @@ class BoschWaterHeater(WaterHeaterDevice):
     async def async_set_temperature(self, **kwargs):
         """Set new target temperature."""
         target_temp = kwargs.get(ATTR_TEMPERATURE)
-        if target_temp:
+        if target_temp and target_temp != self._target_temperature:
             await self._dhw.set_temperature(target_temp)
         else:
             _LOGGER.error("A target temperature must be provided")
 
     async def async_set_operation_mode(self, operation_mode):
         """Set operation mode."""
-        op_mode_to_set = self._states_conv.get(operation_mode)
-        if op_mode_to_set:
-            await self._dhw.set_operation_mode(op_mode_to_set)
+        op_mode = self._hastates.get(operation_mode)
+        _LOGGER.debug(f"Setting operation mode {operation_mode} which is Bosch {op_mode}.")
+        if op_mode and operation_mode != self._mode and operation_mode in self._operation_list:
+            new_mode = await self._dhw.set_operation_mode(op_mode)
+            _LOGGER.debug(f"Set operation mode to {new_mode}.")
         else:
-            _LOGGER.error("An operation mode must be provided")
+            _LOGGER.error(f"Correct operation mode must be provided. I got {operation_mode}")
 
     def update(self):
         """Get the latest date."""
-        if (not self._dhw or not self._dhw.json_scheme_ready or
-                not self._dhw.update_initialized):
+        _LOGGER.debug("Updating Bosch water_heater.")
+        if (
+            not self._dhw
+            or not self._dhw.json_scheme_ready
+            or not self._dhw.update_initialized
+        ):
             return
         self._state = self._dhw.get_value(STATUS)
         curr_temp = self._dhw.get_property(CURRENT_TEMP)
         self._current_temperature = curr_temp.get(self._dhw.strings.val)
         self._temperature_units = UNITS_CONVERTER.get(
-            curr_temp.get(self._dhw.strings.units, 'C'))
-        (self._target_temperature, self._low_temp, self._max_temp) =\
-            self._dhw.target_temperature
+            curr_temp.get(self._dhw.strings.units, "C")
+        )
+        (
+            self._target_temperature,
+            self._low_temp,
+            self._max_temp,
+        ) = self._dhw.target_temperature
         # self._holiday_mode = self._dhw.get_value(HC_HOLIDAY_MODE)
         mode = self._dhw.get_property(OPERATION_MODE)
         self._operation_list = self.operation_list_converter(
-            mode.get(self._dhw.strings.allowed_values))
-        self._mode = self._states_conv_inv.get(
-            mode.get(self._dhw.strings.val, "").lower(), STATE_OFF)
+            mode.get(self._dhw.strings.allowed_values)
+        )
+        self._mode = self._hastates_inv.get(
+            mode.get(self._dhw.strings.val), STATE_OFF)
         self._target_temp_off = self._dhw.get_value(WATER_OFF)
         if self._update_init:
             self._update_init = False
             self.async_schedule_update_ha_state()
 
     def operation_list_converter(self, op_list):
-        return [self._states_conv_inv.get(value.lower()) for value in op_list]
+        return [self._hastates_inv.get(value) for value in op_list]
 
     @property
     def current_temperature(self):
@@ -214,16 +227,6 @@ class BoschWaterHeater(WaterHeaterDevice):
     def target_temperature(self):
         """Return the temperature we try to reach."""
         return self._target_temperature
-
-    # @property
-    # def target_temperature_high(self):
-    #     """Return the temperature we try to reach."""
-    #     return self._target_temperature
-
-    # @property
-    # def target_temperature_low(self):
-    #     """Return the temperature we try to reach."""
-    #     return self._target_temp_off
 
     @property
     def min_temp(self):
