5dd6ef56cf5752a425c8de618ca4a7d00ce352f7
jfhautenauven <jf.hautenauven@gmail.com>
2026-07-22 17:14:57 +0200

    Avoid blocking SSL setup in event loop

--- custom_components/bosch/__init__.py
+++ custom_components/bosch/__init__.py
@@ -6,6 +6,7 @@ import logging
 import random
 from collections.abc import Awaitable
 from datetime import timedelta
+from functools import partial
 from typing import Any
 
 import homeassistant.helpers.config_validation as cv
@@ -236,14 +237,17 @@ class BoschGatewayEntry:
         _LOGGER.debug("Initializing Bosch integration.")
         self._update_lock = asyncio.Lock()
         BoschGateway = bosch.gateway_chooser(device_type=self._device_type)
-        self.gateway = BoschGateway(
-            session=async_get_clientsession(self.hass, verify_ssl=False)
-            if self._protocol == HTTP
-            else None,
-            session_type=self._protocol,
-            host=self._host,
-            access_key=self._access_key,
-            access_token=self._access_token,
+        self.gateway = await self.hass.async_add_executor_job(
+            partial(
+                BoschGateway,
+                session=async_get_clientsession(self.hass, verify_ssl=False)
+                if self._protocol == HTTP
+                else None,
+                session_type=self._protocol,
+                host=self._host,
+                access_key=self._access_key,
+                access_token=self._access_token,
+            )
         )
 
         async def close_connection(event) -> None:
